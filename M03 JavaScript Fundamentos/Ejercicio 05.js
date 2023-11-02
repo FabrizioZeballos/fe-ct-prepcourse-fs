bb9d26819a9dbbdc52524ad7d937aed27d913dd0
Fabrizio Zeballos <139596509+FabrizioZeballos@users.noreply.github.com>
2023-11-02 15:17:46 -0300

    Update Ejercicio 05.js

--- M03 JavaScript Fundamentos/Ejercicio 05.js
+++ M03 JavaScript Fundamentos/Ejercicio 05.js
@@ -7,7 +7,7 @@ function esPositivo(num) {
    // Si el número es positivo ---> "Es positivo".
    // Si el número es negativo ---> "Es negativo".
    // Si el número es 0, devuelve false.
-   // Tu código:
+   // Tu código: if (num >= 0) {return "Es positivo"} else if (num < 0) {return "Es negativo"} else if (num === 0) {return 'false'};
 }
 
 function agregarSimboloExclamacion(str) {
